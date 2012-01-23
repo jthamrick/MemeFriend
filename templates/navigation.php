9785a746569c4f37c8ce841c956c6ef0db6687d3
JT Hamrick <jt@the-jlgroup.com>
2012-01-22 21:22:02 -0600

    Link and read me updates

--- templates/navigation.php
+++ templates/navigation.php
@@ -17,7 +17,7 @@
 	<ul>
 		<li><a href="create.php" id="create">My Images</a></li>
 		<li><a href="create-friend.php" id="create-friend">Friends Images</a></li>
-		<li><a href="http://memefriend.com/sandbox/upload-image" id="upload">Upload Image</a></li>
+		<li><a href="upload-image.php" id="upload">Upload Image</a></li>
 	</ul>
 
 	<h2 class="nav-padding">Filter by</h2>
@@ -34,11 +34,11 @@
 	<h2 class="nav-padding">Connect with us</h2>
 	<div class="divider"></div>
 	<ul>
-		<li><a href="http://www.facebook.com/pages/memefriendcom/249289061793991?sk=wall">Facebook</a></li>
-		<li><a href="https://twitter.com/#!/memefriend">Twitter</a></li>
+		<li><a href="#">Facebook</a></li>
+		<li><a href="#">Twitter</a></li>
 	</ul>
 	
-	<h2 class="nav-padding other"><a href="http://memefriend.com/blog" id="blog">Blog</a></h2>
+	<h2 class="nav-padding other"><a href="#" id="blog">Blog</a></h2>
 	<h2 class="other"><a href="about.php" id="about">About</a></h2>
 	<h2 class="other"><a href="privacy.php" id="privacy">Privacy</a></h2>
 	<h2 class="other"><a href="terms.php" id="terms">Terms</a></h2>
